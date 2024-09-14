fdb2e98405b67090c2fc4663c819b0f9decc4bbc
EvetteFarag <evette.farag98@gmail.com>
2024-09-14 04:42:43 +0300

    Add files via upload

--- udemy_dashboard.py
+++ udemy_dashboard.py
@@ -38,7 +38,7 @@ st.sidebar.markdown("Made with :heart_eyes: by [Evette Farag](www.linkedin.com/i
 a1 , a2 , a3  = st.columns([3,3,3])
 
 with a1:
-    a1.metric("No. of courses" ,df["course_id"].value_counts().sum())
+    a1.metric("No. of courses" ,df["course_title"].value_counts().sum())
 with a2:
     # Calculate total profit
     sum_profit = df["Profit"].sum()
